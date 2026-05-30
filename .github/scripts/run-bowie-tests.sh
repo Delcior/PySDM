aff69781df55c28cec09f1919891d0d779f00a90
Kacper Derlatka <51274280+Delcior@users.noreply.github.com>
2026-05-31 01:02:12 +0200

    Add bowie (#6)
    * dont compile numpy
    
    * dont compile numpy
    
    * remove CI env
    
    * check CI presence

--- .github/scripts/run-bowie-tests.sh
+++ .github/scripts/run-bowie-tests.sh
@@ -18,8 +18,10 @@ python -m venv ./.venv
 source ./.venv/bin/activate
 export PATH=/mnt/cluster-workspace/bin/openmpi-5.0.10/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/cluster-workspace/bin/openmpi-5.0.10/lib
+unset CI
 
-CI=1 python -m pip install -e '.[unit-tests,CI_version_pins]'
+python -c 'import os; print("CI during install:", os.environ.get("CI"))'
+env -u CI python -m pip install -e '.[unit-tests]'
 mpiexec --mca btl_tcp_if_include eth0 -n 2 $2
 exit_code=$?
 echo "Tests completed with exit code $exit_code"
