3b42188ab82a2843e1a9b921e3cf72f876ccd45f
Delcior <kacper.derlatka@outlook.com>
2026-05-31 00:55:53 +0200

    check CI presence

--- .github/scripts/run-bowie-tests.sh
+++ .github/scripts/run-bowie-tests.sh
@@ -18,7 +18,9 @@ python -m venv ./.venv
 source ./.venv/bin/activate
 export PATH=/mnt/cluster-workspace/bin/openmpi-5.0.10/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/cluster-workspace/bin/openmpi-5.0.10/lib
+unset CI
 
+python -c 'import os; print("CI during install:", os.environ.get("CI"))'
 env -u CI python -m pip install -e '.[unit-tests]'
 mpiexec --mca btl_tcp_if_include eth0 -n 2 $2
 exit_code=$?
