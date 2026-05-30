d54f5e009ec72d835553da06100fd53e2ce5b7c8
Delcior <kacper.derlatka@outlook.com>
2026-05-31 00:33:59 +0200

    dont compile numpy

--- .github/scripts/run-bowie-tests.sh
+++ .github/scripts/run-bowie-tests.sh
@@ -19,7 +19,7 @@ source ./.venv/bin/activate
 export PATH=/mnt/cluster-workspace/bin/openmpi-5.0.10/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/cluster-workspace/bin/openmpi-5.0.10/lib
 
-CI=1 python -m pip install -e '.[unit-tests,CI_version_pins]'
+CI=1 python -m pip install -e '.[unit-tests]'
 mpiexec --mca btl_tcp_if_include eth0 -n 2 $2
 exit_code=$?
 echo "Tests completed with exit code $exit_code"
