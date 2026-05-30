5956675d97ea944a1ea9fd55d0b7231ef4fa7541
Delcior <kacper.derlatka@outlook.com>
2026-05-31 00:44:41 +0200

    remove CI env

--- .github/scripts/run-bowie-tests.sh
+++ .github/scripts/run-bowie-tests.sh
@@ -19,7 +19,7 @@ source ./.venv/bin/activate
 export PATH=/mnt/cluster-workspace/bin/openmpi-5.0.10/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/cluster-workspace/bin/openmpi-5.0.10/lib
 
-python -m pip install -e '.[unit-tests]'
+env -u CI python -m pip install -e '.[unit-tests]'
 mpiexec --mca btl_tcp_if_include eth0 -n 2 $2
 exit_code=$?
 echo "Tests completed with exit code $exit_code"
